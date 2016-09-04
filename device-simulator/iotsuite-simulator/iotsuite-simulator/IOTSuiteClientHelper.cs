0fdaa7a61c09da255e9d25b8bca4f0756e954931
Michael Chi <michi@microsoft.com>
2016-09-04 21:37:21 +0800

    add documents

--- device-simulator/iotsuite-simulator/iotsuite-simulator/IOTSuiteClientHelper.cs
+++ device-simulator/iotsuite-simulator/iotsuite-simulator/IOTSuiteClientHelper.cs
@@ -87,24 +87,54 @@ namespace simulator
 
     public class IOTSuiteTelemetryData
     {
+        /*
+         * {“_0”：“324”，“_100”：“324”，“_101”：“324”，“_102”：“324”}
+格式说明：{“0代表对应数值为设备ID”：“设备ID”，“协议地址A”：“电压”，“协议地址B”：“电流”“协议地址B”：“功率”}这一个代表一帧
+    */
         public string DeviceId { get; set; }
-        public int _0101 { get; set; }
-        public int _0102 { get; set; }
-        public int _0103 { get; set; }
-        public int _0104 { get; set; }
-        public int _0105 { get; set; }
+        //设备ID”
+        public int _0 { get; set; }
+        //电压
+        public int _100 { get; set; }
+        //电流
+        public int _101 { get; set; }
+        //功率
+        public int _102 { get; set; }
 
+        //故障代碼
+        public int _0106 { get; set; }
+        //狀態 0:故障, 1:關網, 2:離網, 3:待機
+        public int? _104 { get; set; }
+
+        const int _106_Change_Interval = 10;
+        static int _106_Changed = 0;
+        static int _106_LastValue = 0;
+        private static int GetErrorCode()
+        {
+            var random = new Random((int)DateTime.Now.Ticks);
+            if (random.Next(0, 101) > 80) return 0;
+            if (random.Next(0, 101) > 50) return 1001;
+            return 1002;
+        }
         public static string Random(string deviceId)
         {
             IOTSuiteTelemetryData data = new IOTSuiteTelemetryData();
             var random = new Random((int)DateTime.Now.Ticks);
             data.DeviceId = deviceId;
-            data._0101 = random.Next(0, 4);//1
-            data._0102 = random.Next(0, 4);//2;
-            data._0103 = random.Next(23, 33);//32;
-            data._0104 = random.Next(36, 46);//45;
-            data._0104 = random.Next(78, 88);//87;
-
+            data._0 = random.Next(300, 400);//1
+            data._100 = random.Next(300, 400);//2;
+            data._101 = random.Next(300, 400);//32;
+            data._102 = random.Next(300, 400);//45;
+            data._0106 = (_106_Changed % _106_Change_Interval == 0 ? GetErrorCode(): _106_LastValue);
+            _106_LastValue = data._0106;
+            if (data._0106 == 1001 || data._0106 == 1002)
+            {
+                data._104 = 0;
+            }
+            else if(data._0106 == 0)
+            {
+                data._104 = random.Next(1, 4);
+            }
             return JsonConvert.SerializeObject(data);
         }
     }
@@ -121,13 +151,13 @@ namespace simulator
 
             device.HubEnabledState = true;
             device.DeviceID = deviceId;
-            device.Manufacturer = "Microsoft";
-            device.ModelNumber = "Lumia950";
+            device.Manufacturer = "BYD";
+            device.ModelNumber = "RTU-001";
             device.SerialNumber = "5849735293875";
             device.FirmwareVersion = "10";
-            device.Platform = "Windows 10";
-            device.Processor = "SnapDragon";
-            device.InstalledRAM = "3GB";
+            device.Platform = "uC/OS-II";
+            device.Processor = "ARM M4";
+            device.InstalledRAM = "64 MB";
             device.DeviceState = "normal";
 
             thermostat.DeviceProperties = device;
@@ -139,36 +169,30 @@ namespace simulator
             TriggerAlarm.Parameters = new CommandParameter[] { param };
 
             Command demoCommand = new Command();
-            demoCommand.Name = "DemoCommand";
+            demoCommand.Name = "DemoCommand2";
             CommandParameter demoParam = new CommandParameter();
             demoParam.Name = "CommandType";
             demoParam.Type = "String";
-            demoCommand.Parameters = new CommandParameter[] { demoParam };
+            CommandParameter demoParam2 = new CommandParameter();
+            demoParam2.Name = "CommandType2";
+            demoParam2.Type = "String";
+            demoCommand.Parameters = new CommandParameter[] { demoParam, demoParam2 };
 
             thermostat.Commands = new Command[] { TriggerAlarm, demoCommand };
             thermostat.Telemetry = new TelemetryMember[] {
-                new TelemetryMember {
-                    Name = "_0101",
-                    Type = "int",
-                    DisplayName = "工作狀態"
-                },
+                
              new TelemetryMember {
-                    Name = "_0102",
-                    Type = "int",
-                    DisplayName = "工作模式"
-                },
-            new TelemetryMember {
-                    Name = "_0103",
+                    Name = "_100",
                     Type = "int",
                     DisplayName = "電壓"
                 },
             new TelemetryMember {
-                    Name = "_0104",
+                    Name = "_101",
                     Type = "int",
                     DisplayName = "電流"
                 },
             new TelemetryMember {
-                    Name = "_0105",
+                    Name = "_102",
                     Type = "int",
                     DisplayName = "功率"
                 }};
